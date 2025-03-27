308f7b9cf6c2ecc04216bda7b829d2d0d73e62ee
Kartik Narayan Sahoo <kartik.sahoo26@gmail.com>
2025-03-27 14:39:09 +0530

    add: underline invalidpage

--- frontend/src/pages/InvalidPage.tsx
+++ frontend/src/pages/InvalidPage.tsx
@@ -6,7 +6,7 @@ export const InvalidPage = () => {
     return (
         <div role="status" className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
             <div className="text-3xl">{"This page does not exist :("}</div>
-            <div className="text-lg text-center">Go to <Link className="hover:underline" to={'/signin'}>login</Link></div>
+            <div className="text-lg text-center">Go to <Link className="underline" to={'/signin'}>login</Link></div>
         </div>
     )
 }
